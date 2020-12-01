b42acb07941d498f3a48e0c2ea556be79edeb591
Offerel <14041522+Offerel@users.noreply.github.com>
2020-12-01 21:37:10 +0100

    Fixed adding new folder

--- index.php
+++ index.php
@@ -912,7 +912,7 @@ function addFolder($database, $ud, $bm) {
 	}
 	
 	e_log(8,"Get folder data for adding folder");
-	$query = "SELECT IFNULL(MAX(`bmIndex`),-1) + 1 AS `nindex`, `bmParentId` FROM `bookmarks` WHERE `bmParentId` = '".$bm['folder']."';";
+	$query = "SELECT IFNULL(MAX(`bmIndex`),-1) + 1 AS `nindex`, `bmParentId` FROM `bookmarks` WHERE `bmParentId` = '".$bm['folder']."' AND `userID` = ".$ud['userID'].";";
 	$statement = $db->prepare($query);
 	e_log(9,$query);
 	
